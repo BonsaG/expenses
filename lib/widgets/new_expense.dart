a914ce124159e8a30bc345c0e85e1938f485c00c
Bonsa girma <bonsogirma@gmail.com>
2026-01-15 17:17:23 +0300

    adding drop down for category

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -15,6 +17,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -84,10 +87,29 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                .map(
+                  (category) => DropdownMenuItem(
+                    value: category,
+                    child: Text(
+                      category.name.toUpperCase()
+                      )
+                      )
+                      ).toList(),
+                 onChanged:(value) {
+                  setState(() {
+                    _selectedCategory = value!;
+                  });
+                 },
+                 ), 
+                const Spacer(),
               TextButton(
-                onPressed: () {
+                onPressed: () { 
                   Navigator.pop(context);
                 },
                 child: const Text('Cancel'),
@@ -96,6 +118,7 @@ class _NewExpenseState extends State<NewExpense> {
                 onPressed: () {
                   print(_titleController.text);
                   print(_amountController.text);
+                  print(_selectedCategory); 
                 },
                 child: const Text('Save Expense'),
               ),
